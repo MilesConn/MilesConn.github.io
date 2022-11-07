e237fcc12a8d4c45dacdf37256929c2a96b51b23
Miles Conn <miles@ponto.org>
2022-11-06 23:06:44 -0500

    Cube rotates

---
+++ blog/src/components/3d/Cube.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { Mesh, useFrame } from "@threlte/core";
+  import { BoxGeometry, MeshBasicMaterial } from "three";
+
+  let rotation = 0;
+  useFrame(() => {
+    rotation += 0.01;
+  });
+</script>
+
+<Mesh
+  rotation={{ x: rotation, y: rotation, z: rotation }}
+  geometry={new BoxGeometry(1, 1, 1)}
+  material={new MeshBasicMaterial()}
+/>
