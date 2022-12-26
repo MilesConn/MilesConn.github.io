5cd167a6a6b82c98906d5ea2deb8b5398b519c8c
Miles Conn <miles@ponto.org>
2022-12-26 14:25:51 -0500

    Updated search

--- blog/scripts/search/prepare-index.js
+++ blog/scripts/search/prepare-index.js
@@ -1,37 +1,48 @@
-import path from 'path'
-import { promises as fs } from 'fs'
-import { globby } from 'globby'
-import grayMatter from 'gray-matter'
+import path from "path";
+import { promises as fs } from "fs";
+import { globby } from "globby";
+import grayMatter from "gray-matter";
 
 (async function () {
-    // prepare the dirs
-    const srcDir = path.join(process.cwd(), 'src')
-    const publicDir = path.join(process.cwd(), 'public')
-    const contentDir = path.join(srcDir, 'pages', 'blog')
-    const contentFilePattern = path.join(contentDir, '*.md')
-    const indexFile = path.join(publicDir, 'search-index.json')
-    const getSlugFromPathname = (pathname) => path.basename(pathname, path.extname(pathname))
+  // prepare the dirs
+  const blogSrcDir = "../";
+  const srcDir = path.join(blogSrcDir, "src");
+  const publicDir = path.join(blogSrcDir, "public");
+  const contentDir = path.join(srcDir, "pages", "blog");
+  const contentFilePattern = path.join(contentDir, "*.md");
+  const indexFile = path.join(publicDir, "search-index.json");
+  const getSlugFromPathname = (pathname) =>
+    path.basename(pathname, path.extname(pathname));
 
-    const contentFilePaths = await globby([ contentFilePattern ])
+  const contentFilePaths = await globby([contentFilePattern]);
 
-    if(contentFilePaths.length) {
-        const files = contentFilePaths.map(async(filePath) => await fs.readFile(filePath, 'utf8'))
-        const index = []
-        let i = 0
-        for await (let file of files){
-            const { data: { title, description, tags }, content } = grayMatter(file)
-            index.push({
-                slug: getSlugFromPathname(contentFilePaths[i]),
-                category: 'blog',
-                title,
-                description,
-                tags,
-                body: content
-            })
-            i++
-        }
-        await fs.writeFile(indexFile, JSON.stringify(index))
-        console.log(`Indexed ${index.length} documents from ${contentDir} to ${indexFile}`)
+  console.log("Found the following content: ", contentFilePaths);
+  console.log("Generating slug.....");
+  if (contentFilePaths.length) {
+    const files = contentFilePaths.map(
+      async (filePath) => await fs.readFile(filePath, "utf8")
+    );
+    const index = [];
+    let i = 0;
+    for await (let file of files) {
+      const {
+        data: { title, description, tags },
+        content,
+      } = grayMatter(file);
+      index.push({
+        slug: getSlugFromPathname(contentFilePaths[i]),
+        category: "blog",
+        title,
+        description,
+        tags,
+        body: content,
+      });
+      i++;
     }
-
+    await fs.writeFile(indexFile, JSON.stringify(index));
+    console.log(
+      `Indexed ${index.length} documents from ${contentDir} to ${indexFile}`
+    );
+  }
+  console.log("Done.");
 })();
