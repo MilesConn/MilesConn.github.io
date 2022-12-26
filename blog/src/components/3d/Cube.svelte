40ba7efccf80b973e263af8e83c71d2b685316aa
Miles Conn <miles@ponto.org>
2022-12-26 13:47:31 -0500

    Figured out how to lerp colors

--- blog/src/components/3d/Cube.svelte
+++ blog/src/components/3d/Cube.svelte
@@ -5,17 +5,56 @@
   const vertex = document.querySelector("#vert").textContent.trim();
   const fragment = document.querySelector("#frag").textContent.trim();
 
+  const color1 = new Color(0xff6c00);
+  const color2 = new Color(0 / 255, 180 / 255, 255 / 255);
+
   const ColorShiftMaterial = new ShaderMaterial({
     uniforms: {
       time: { value: 0 },
-      color: { value: new Color(0.05, 0.2, 0.025) },
+      color: { value: color1.clone() },
     },
     vertexShader: vertex,
     fragmentShader: fragment,
   });
 
   let rotation = 0;
+  let lerp = 0;
+  let towardsPositive = true;
+  const orError = (msg: string) => {
+    throw new Error(msg);
+  };
+
+  function* range(start = 0, end = 1, step = 0.01) {
+    const steps = Math.abs((end - start) / step);
+    Number.isInteger(steps) || orError("Only finite steps supported");
+
+    const dir = end > start;
+
+    for (let i = 0; i < steps; i++) {
+      yield start;
+      if (dir) {
+        start += step;
+      } else {
+        start -= step;
+      }
+    }
+  }
+
+  function* pingpong(start = 0, end = 1, step = 0.01) {
+    while (true) {
+      yield* range(start, end, step);
+      yield* range(end, start, step);
+    }
+  }
+
+  const lerpGen = pingpong();
+
   useFrame(() => {
+    ColorShiftMaterial.uniforms.color.value.lerpColors(
+      color1,
+      color2,
+      lerpGen.next().value
+    );
     rotation += 0.01;
   });
 </script>
