0b97747215d3d883d0413b0ee38e9a82f0b92860
Miles Conn <milesjconn@gmail.com>
2023-09-03 15:25:03 -0400

    Footer almost matches mockup perfectly

--- blog/src/components/Icons/Github.svelte
+++ blog/src/components/Icons/Github.svelte
@@ -1,9 +1,9 @@
----
+<script>
   export let size = "50px";
   export let strokeWidth = "1.1";
   export let color = "#000000";
   export let fill = "none";
----
+</script>
 
 <svg
   width={size}
@@ -16,6 +16,5 @@
   ><path
     d="M16 22.027v-2.87a3.37 3.37 0 00-.94-2.61c3.14-.35 6.44-1.54 6.44-7a5.44 5.44 0 00-1.5-3.75 5.07 5.07 0 00-.09-3.77s-1.18-.35-3.91 1.48a13.38 13.38 0 00-7 0c-2.73-1.83-3.91-1.48-3.91-1.48A5.07 5.07 0 005 5.797a5.44 5.44 0 00-1.5 3.78c0 5.42 3.3 6.61 6.44 7a3.37 3.37 0 00-.94 2.58v2.87M9 20.027c-3 .973-5.5 0-7-3"
     stroke-linecap="round"
-    stroke-linejoin="round"
-  /></svg
+    stroke-linejoin="round"></path></svg
 >
